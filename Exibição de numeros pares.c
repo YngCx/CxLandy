358764a18b40215a8eaec6a62360fbe47de43bf3
YngCx <159449773+YngCx@users.noreply.github.com>
2024-03-07 14:40:32 -0300

    Add files via upload
    Exibindo numeros pares

---
+++ Exibição de numeros pares.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+	
+	int i;
+
+printf("EXIBINDO OS NUMERO PARES: ");
+
+	for(i = 100; i <= 120; i+=2){
+			printf("%i \n", i);
+	}
+
+	return 0;	
+
+}
