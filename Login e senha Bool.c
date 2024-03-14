0fa01edce452869880bad3260c50b9d37094f65b
YngCx <159449773+YngCx@users.noreply.github.com>
2024-03-14 14:25:49 -0300

    Add files via upload

---
+++ Login e senha Bool.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main(void){
+	setlocale(LC_ALL,"");
+	
+	char loginCad[200] = "Alvaro";
+	char senhaCad[200] =  "Alvaro12";
+	char login[200];
+	char senha[200];
+	bool loginCorreto;
+	bool senhaCorreta;
+	
+	do{
+		printf("\n-------------------------\n");
+		printf("Digite seu usuario: ");
+		scanf("%s",&login);
+		fflush(stdin);
+	
+		printf("Digite sua senha: ");
+		scanf("%s",&senha);
+		
+		loginCorreto = strcmp(loginCad, login) == 0;
+		senhaCorreta = strcmp(senhaCad, senha) == 0;
+		
+	
+	}while (!loginCorreto || !senhaCorreta);
+	
+		printf("\nAcessando o sistema...!");
+	return 0;
+	
+}
