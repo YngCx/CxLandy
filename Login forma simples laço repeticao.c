22cd142a9275d4573a6725cd9fda9681c54476c8
YngCx <159449773+YngCx@users.noreply.github.com>
2024-03-14 14:07:02 -0300

    Add files via upload

---
+++ Login forma simples laço repeticao.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+	char logincada[200] = "caio";
+	char senhacada[200] = "1234";
+	char login[200];
+	char senha[200];
+	
+		do{
+	
+		
+
+		printf("\nDigite o login do usuario: ");
+		scanf("%s",&login);
+
+		printf("\nDigite a senha do usuario: ");
+		scanf("%s",&senha);		
+		
+		} while (strcmp(logincada, login ) != 0 && strcmp(senhacada, senha ) != 0);
+		 
+		 printf("\nAcessando o sistema..");
+		
+
+	return 0; 
+
+
+}
