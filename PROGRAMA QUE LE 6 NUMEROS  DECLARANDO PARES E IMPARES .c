18f2738b915dcda315c194f34353c71df6079f4d
YngCx <159449773+YngCx@users.noreply.github.com>
2024-03-15 16:22:50 -0300

    Add files via upload

---
+++ PROGRAMA QUE LE 6 NUMEROS  DECLARANDO PARES E IMPARES .c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <locale.h>
+
+int main() {
+		setlocale(LC_ALL,"");
+		
+		
+	int numeros [6];
+	int nuimpares = 0, numpar = 0;
+	int i;
+	
+	for ( i = 0; i < 6; i++){
+	
+	printf("=== EXIBA AS INFORMACOES ====\n");
+	printf("Digite os %i numeros: ", i+1);
+	scanf("%i",&numeros[i]);
+	system("cls || clear");
+	
+	if (numeros [i] %2 == 0) {
+		numpar++;
+	} else {
+		nuimpares++;
+	}
+	}
+	
+	printf("\nNumero par: %i", numpar);
+	printf("\nNumero impar: %i", nuimpares);
+	return 0;
+	
+}
+
